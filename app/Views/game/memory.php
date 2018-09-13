95c0acfd562af8ebae94f7a36d0b98c6078913d8
Vanda89 <sandrinealcazar@gmail.com>
2018-09-13 15:35:07 +0200

    Correction des games .php + stylisation des themes pour le typeLetter.php

--- app/Views/game/memory.php
+++ app/Views/game/memory.php
@@ -3,9 +3,9 @@
 
 <main class="row mb-5">
   <section id="memory-container" class="game-container container d-flex flex-column justify-content-start align-items-center">
-    <hgroup class="game-header row d-flex flex-row flex-sm-column justify-content-center align-items-center mb-4">
+    <hgroup class="game-header row d-flex flex-row flex-sm-column justify-content-center align-items-center text-center mb-2">
       <h1 id="memory-title" class="game-title text-capitalize">memory</h1>
-      <p class="instructions text-center mt-1 mb-2">Retourne une carte et trouve sa paire.</p>
+      <p class="instructions mt-1 mb-2">Retourne une carte et trouve sa paire.</p>
     </hgroup>
     
   </section>
--- app/Views/game/typeLetter.php
+++ app/Views/game/typeLetter.php
@@ -3,9 +3,9 @@
 
 <main class="row mt-2 mb-5">
   <section id="typeLetter-container" class="game-container container d-flex flex-column justify-content-start align-items-center">
-    <hgroup class="game-header row d-flex flex-row flex-sm-row justify-content-center align-items-center mt-4 mb-5">
+    <hgroup class="game-header row d-flex flex-row flex-sm-row justify-content-center align-items-center text-center mt-4 mb-5">
       <h1 id="typeLetter-title" class="game-title text-capitalize mb-3">Tape-lettre</h1>
-      <p class="instructions text-center">Retrouve la même lettre sur ton clavier et appuie dessus.</p>
+      <p class="instructions">Retrouve la même lettre sur ton clavier et appuie dessus.</p>
     </hgroup>
 
 
